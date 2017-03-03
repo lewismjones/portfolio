9e129506f20ae1ad380f234e64463630de22e627
Lewis Jones <lewismjones@gmail.com>
2017-03-03 00:17:02 -0500

    trying again

--- public/portfolioserver.rb
+++ public/portfolioserver.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 
-    get '/' do
-
-'portfolio.html'
+get '/' do
+  File.read(File.join('public', 'index.html'))
+  # or
+  # send_file File.join(settings.public, 'index.html')
+end
+get '/portfolio.html' do
+  File.read(File.join('public', 'portfolio.html'))
+  # or
+  # send_file File.join(settings.public, 'index.html')
 end
